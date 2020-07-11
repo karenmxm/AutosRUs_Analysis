c6a1890002a5be03ed3a5d0d09931726b8c4a830
mengxiaomeng01 <mengxiao23@gmail.com>
2020-07-11 10:32:01 -0500

    message

---
+++ MechaCarChallenge.R
@@ -0,0 +1,38 @@
+# Read MechaCar_mpg dataset
+MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv',
+                         check.names = F,
+                         stringsAsFactors = F)
+head(MechaCar_mpg)
+
+# Change column names
+colnames(MechaCar_mpg)[1] <- "vehicle_length"
+colnames(MechaCar_mpg)[2] <- "vehicle_weight"
+colnames(MechaCar_mpg)[3] <- "spoiler_angle"
+colnames(MechaCar_mpg)[4] <- "ground_clearance"
+
+cor_matrix <- cor(MechaCar_mpg)
+
+#generate multiple linear regression model
+lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance + AWD,
+   data=MechaCar_mpg)
+
+#generate summary statistics
+summary(lm(mpg ~ vehicle_length + vehicle_weight + ground_clearance + AWD,
+           data=MechaCar_mpg)) 
+
+
+# Read Suspension_Coil dataset
+Suspension_Coil <- read.csv(file = 'Suspension_Coil.csv',
+                            check.names = F,
+                            stringsAsFactors = F)
+summary(Suspension_Coil$PSI)
+var(Suspension_Coil$PSI)
+sd(Suspension_Coil$PSI)
+
+#compare means of data and 1,500
+t.test(log10(Suspension_Coil$PSI),mu = 1500) 
+
+
+
+
+
